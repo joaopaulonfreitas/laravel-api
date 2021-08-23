f099aa1bd8f994d59d7177f95a70811e48ce4d72
João Paulo N. Freias <joaopaulonfreitas@gmail.com>
2021-08-23 18:18:33 -0300

    Add route to show Products, creating Model, Factory, Seeder, Resource and how to use Category Resource inside of the Product Resource

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,3 +23,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('categories', [CategoryController::class, 'index'])->name('category.all');
 Route::get('categories/{category}', [CategoryController::class, 'show'])->name('category.show');
+
+
+Route::get('products', [ProductController::class, 'index'])->name('products.all');
